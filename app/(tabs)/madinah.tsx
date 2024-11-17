5966236ee9e5072e527762112b60e3b4844405a0
pmudassir <pmuddasir@gmail.com>
2024-11-17 17:45:23 +0530

    nativewind config

---
+++ app/(tabs)/madinah.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Madinah = () => {
+  return (
+    <View>
+      <Text>Madinah</Text>
+    </View>
+  );
+};
+
+export default Madinah;
